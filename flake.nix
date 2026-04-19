b54f1f5a5e85a3c4a8a076fcdd6c5e0d85f107d4
countgitmick <countgitmick@email.com>
2026-04-19 15:31:22 -0700

    fix(nix): update npmDepsHash for globals 17.5.0 bump

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
 
             src = ./.;
 
-            npmDepsHash = "sha256-BFCngfBbPEw4M17soUMSHzG2rGvFZoEHU3qrdar0tAg=";
+            npmDepsHash = "sha256-MEqMVXJ2X62DpDpB1XoFFSBowAZOuY/t8D6ZboZdKaw=";
             npmDepsFetcherVersion = 2;
             npmFlags = [ "--legacy-peer-deps" ];
             makeCacheWritable = true;
