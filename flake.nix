7d1ae0f4d1c7aa2c7772dd4936ed2a6f7db9dee4
countgitmick <countgitmick@email.com>
2026-04-19 15:32:55 -0700

    fix(nix): update npmDepsHash for shaka-player 5.1.0 bump

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
 
             src = ./.;
 
-            npmDepsHash = "sha256-BFCngfBbPEw4M17soUMSHzG2rGvFZoEHU3qrdar0tAg=";
+            npmDepsHash = "sha256-O4Bz66DUXAgrsSTglveDHEZn7AMcC7BYFTSrnHSRtdo=";
             npmDepsFetcherVersion = 2;
             npmFlags = [ "--legacy-peer-deps" ];
             makeCacheWritable = true;
