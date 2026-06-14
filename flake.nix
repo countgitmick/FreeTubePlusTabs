591fd9bec99d0493ceca0d841559c64abf066dbb
countgitmick <countgitmick@email.com>
2026-06-13 19:34:58 -0700

    chore: release v0.24.7
    Bug-fix release for the modifier-click and tab navigation issues reported by @raindropsfromsky:
    
    - #112/#113: Ctrl/Cmd+click on a video opens a background tab instead of a new window
    - #116: Shift+click opens a new window with just that video; deep-linked windows no longer clone or overwrite the saved session
    - #111: new tabs clear the address bar
    
    Also includes the calibreapp/image-actions CI bump (#152).

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           default = pkgs.stdenv.mkDerivation rec {
             pname = "freetube-plus-tabs";
-            version = "0.24.6";
+            version = "0.24.7";
 
             src = ./.;
 
